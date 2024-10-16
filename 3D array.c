f3d6379adf8a96926e61a1860f31932c62d1d219
ALEXANDER MUTHIANI <muthianialexander339@gmail.com>
2024-10-16 08:42:13 +0300

    Add files via upload

---
+++ 3D array.c
@@ -0,0 +1,22 @@
+//array
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	int sum;
+	int marks[2][3]={ {{54,74,86},{38,76,82}},
+                 	{{56,76,98},{54,76,98}} };
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			for(k=0;k<3;k++){
+
+		
+	printf("marks [%d][%d][%d]=%d\n",i,j,k,marks[i][j]);
+
+			}
+		}
+}
+
+	return 0;
+	
+}
